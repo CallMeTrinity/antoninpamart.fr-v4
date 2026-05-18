30420575b0ac1e854c1c9797a47c9bef8aa621b5
CallMeTrinity <anteur.ap@gmail.com>
2026-05-18 17:35:42 +0200

    feat : Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const time = ref();
+setInterval(() => {
+  time.value = new Date().toLocaleTimeString("fr-FR", { hour: "2-digit", minute: "2-digit" }).replace(",", "");
+}, 1000)
+
+const nav : { href: string, text: string}[] = [
+    { href: "#", text: "Travaux" },
+    { href: "#", text: "A propos" },
+    { href: "#", text: "En ce moment" },
+    { href: "#", text: "Contact" },
+];
+</script>
+
+<template>
+  <header class="fixed top-0 left-0 right-0 font-jet text-xs uppercase flex justify-between items-center p-4 px-8">
+    <div>
+      Antonin Pamart · Développeur
+    </div>
+    <div>{{ time }} GMT </div>
+    <nav>
+      <ul class="flex gap-4">
+        <li v-for="(item, index) in nav" :key="index">
+          <a :href="item.href">{{item.text}}</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import type { Component } from 'vue'
+import Header from "@/components/Header.vue";
+
+const sections: Component[] = [
+  Header
+];
+</script>
+
+<template>
+  <div>
+    <div v-for="(section, index) in sections" :key="index">
+      <component :is="section"/>
+    </div>
+  </div>
+</template>
